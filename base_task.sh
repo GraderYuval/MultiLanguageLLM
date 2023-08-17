d97a98f95a12675c050570c9b5b88cb0441f4bc4
noamatia <noamatia15@gmail.con>
2023-08-18 01:36:53 +0300

    base_task.sh

---
+++ base_task.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --job-name=base_task_%A_%a
+#SBATCH --output=outputs/%A/output_%a.out # redirect stdout
+#SBATCH --error=errors/%A/error_%a.err # redirect stderr
+#SBATCH --partition=studentbatch # (see resources section)
+#SBATCH --time=1 # max time (minutes)
+#SBATCH --signal=USR1@120 # how to end job when time’s up
+#SBATCH --nodes=1 # number of machines
+#SBATCH --ntasks=1 # number of processes
+#SBATCH --mem=50000 # CPU memory (MB)
+#SBATCH --cpus-per-task=4 # CPU cores per process
+#SBATCH --gpus=1 # GPUs in total
+#SBATCH --array=0-14
+
+# Define your list of languages
+languages=("ar" "bg" "de" "el" "en" "es" "fr" "hi" "ru" "sw" "th" "tr" "ur" "vi" "zh")
+
+# Get the language for this job
+language=${languages[$SLURM_ARRAY_TASK_ID]}
+
+# Run your command for the specific language
+echo "Processing language: $language"
+python -m tasks.base_task --language "$language"
